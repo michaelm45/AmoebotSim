d60346b97620c78599bf2f4f3d6c5db9a5a434fe
Joseph Briones <jbriones07@gmail.com>
2019-02-04 20:15:50 -0700

    Added correct lattice to Cartesian calculation
    + Fixed nodeInDir() [again], there was a swap needed for directions 9 & 10
    + hexagonal closest packing to Cartesian calculation in cartesianPos(.)
    + Increased distance of head/tail Marker from particle center to be more visible.

--- interface/simulator.cpp
+++ interface/simulator.cpp
@@ -1,6 +1,6 @@
 #include <QVector3D>
-#include <iostream>
 
+#include "helper/conversion.h"
 #include "interface/simulator.h"
 
 Simulator::Simulator() {
@@ -8,7 +8,7 @@ Simulator::Simulator() {
 }
 
 QList<QVariant> Simulator::getModel() const {
-  QList<QVariant> renderingVec;
+  QList<QVariant> model;
   std::vector<Particle> particles = _system->getParticles();
 
   // For each particle, its head and tail positions are pushed back onto list of
@@ -16,25 +16,41 @@ QList<QVariant> Simulator::getModel() const {
   for (Particle p : particles) {
     Node head = p.head;
     Node tail = p.tail();
-    std::vector<double> headMarker = markerCalculation(p.globalHeadMarkDir(), head);
-    std::vector<double> tailMarker = markerCalculation(p.globalTailMarkDir(), tail);
+    std::vector<double> headMarker = markerCalculation(p.globalHeadMarkDir(),
+                                                       head);
+    std::vector<double> tailMarker = markerCalculation(p.globalTailMarkDir(),
+                                                       tail);
 
-    renderingVec.push_back(QVariant({QVector3D(head.x, head.y, head.z),
-                                     QVector3D(tail.x, tail.y, tail.z),
-                                     QVector3D(headMarker[0], headMarker[1], headMarker[2]),
-                                     QVector3D(tailMarker[0], tailMarker[1], tailMarker[2])
-                                     }));
+    std::vector<double> headPos = Conversion::cartesianPos({head.x, head.y,
+                                                            head.z});
+    std::vector<double> tailPos = Conversion::cartesianPos({tail.x, tail.y,
+                                                            tail.z});
 
+    QVector3D headCoor(float(headPos.at(0)), float(headPos.at(1)),
+                       float(headPos.at(2)));
+    QVector3D tailCoor(float(tailPos.at(0)), float(tailPos.at(1)),
+                       float(tailPos.at(2)));
+    QVector3D markHCoor(float(headMarker.at(0)), float(headMarker.at(1)),
+                        float(headMarker.at(2)));
+    QVector3D markTCoor(float(tailMarker.at(0)), float(tailMarker.at(1)),
+                        float(tailMarker.at(2)));
+
+    model.push_back(QVariant({headCoor, tailCoor, markHCoor, markTCoor}));
   }
 
-  return renderingVec;
+  return model;
 }
 std::vector<double> Simulator::markerCalculation(int dir, Node marked) const{
   if(dir != -1){
-    Node target = marked.nodeInDir(dir);
-    return {.25 * (marked.x - target.x),
-            .25 * (marked.y - target.y),
-           .25 * (marked.z - target.z)};
+    Node target = marked.nodeInDir(Conversion::intToUInt(dir));
+    static double expansionFactor = .4;
+    std::vector<int> delta = {(marked.x - target.x), (marked.y - target.y),
+                              (marked.z - target.z)};
+    std::vector<double> cartPos = Conversion::cartesianPos({delta.at(0),
+                                                            delta.at(1),
+                                                            delta.at(2)});
+    return {expansionFactor * cartPos.at(0), expansionFactor * cartPos.at(1),
+            expansionFactor * cartPos.at(2)};
   }
   else{
     return {0,0,0};
